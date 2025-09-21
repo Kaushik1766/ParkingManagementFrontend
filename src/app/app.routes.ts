bfb9abbf98b307098bdd6f174ba74e1d468c2f8f
Kaushik Saha <kaushiksaha004@gmail.com>
2025-09-21 23:57:02 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
