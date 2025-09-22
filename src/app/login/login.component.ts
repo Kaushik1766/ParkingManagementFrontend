f2940211856ed155e686f109f07bcf71731b2ab5
Kaushik Saha <kaushiksaha004@gmail.com>
2025-09-22 22:12:22 +0530

    login email password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,13 +4,15 @@ import { Button } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from "primeng/inputtext";
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { Password } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
-  imports: [Button, FormsModule, InputTextModule, FloatLabelModule],
+  imports: [Button, FormsModule, InputTextModule, FloatLabelModule, Password],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  value3 = ""
+  email = ''
+  password = ''
 }
